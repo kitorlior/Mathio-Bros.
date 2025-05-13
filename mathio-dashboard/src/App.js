4782c8a0b85e925dc61ad4bd8f57e557a8201f42
kitorlior <kitorlior@gmail.com>
2025-05-13 18:02:08 +0300

    fix for dashboard

--- mathio-dashboard/src/App.js
+++ mathio-dashboard/src/App.js
@@ -25,7 +25,7 @@ function App() {
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-import Login from './components/Login';
+import Login from './components/LogIn';
 
 export default function App() {
   return (
